7e37e578eecdbc5f004fbe8f43a7b502fb59bdec
Jeppe Møldrup <Jeppemldrup@gmail.com>
2023-02-17 19:46:51 +0100

    Added other components and started building the header toolbar for navigation

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EducationComponent } from './education/education.component';
+import { ProjectlistComponent } from './projectlist/projectlist.component';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    EducationComponent,
+    ProjectlistComponent
   ],
   imports: [
+    MatButtonModule,
+    MatToolbarModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
