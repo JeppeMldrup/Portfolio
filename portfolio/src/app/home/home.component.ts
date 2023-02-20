f23e39c38dd3712337b1b0dada3a42a8e0d2b6f3
Jeppe Møldrup <Jeppemldrup@gmail.com>
2023-02-20 09:55:07 +0100

    Changed the css media blocks to use flexboxes with wrapping instead for the same responsive look but with less css

--- portfolio/src/app/home/home.component.ts
+++ portfolio/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Link } from './link';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  links: Link[] = [
+    new Link('https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png', 'https://github.com/JeppeMldrup', 'GitHub'),
+    new Link('https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/2560px-LinkedIn_Logo.svg.png', 'https://www.linkedin.com/in/jeppe-m%C3%B8ldrup-8780b6169/', 'LinkedIn')
+  ]
 }
---
+++ portfolio/src/app/home/link.ts
@@ -0,0 +1,11 @@
+export class Link{
+    image: string;
+    link: string;
+    text: string;
+
+    public constructor(_image: string, _link: string, _text: string){
+        this.image = _image;
+        this.link = _link;
+        this.text = _text;
+    }
+}
